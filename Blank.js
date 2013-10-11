9b26a5893f9cfefb353692dcfade243bf9092bc3
Tarang Patel <Tarangp@users.noreply.github.com>
2013-10-11 03:00:11 +0200

    Second test

--- Blank.js
+++ Blank.js
@@ -8,7 +8,7 @@ if (Meteor.isClient) {
       // template data, if any, is available in 'this'
       if (typeof console !== 'undefined')
         console.log("You pressed the button");
-      alert("weeee");
+      alert("weeee test again");
     }
   });
 }
